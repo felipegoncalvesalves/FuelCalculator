cb3a9b032505c8ce974595902afeb8850ece2a68
Felipe <felipemorao@icloud.com>
2024-02-08 19:14:30 +0100

    Color updates and data sending correction.

--- app/src/main/java/presentation/MainActivity.kt
+++ app/src/main/java/presentation/MainActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.fuelcalculatortest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
 
         val comparador : Button = findViewById(R.id.comparar)
         val calculadora : Button = findViewById(R.id.calcular)
